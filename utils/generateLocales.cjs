0fad9b960678a9559081d2368c9557945ae617c0
tomrcc <tom@cloudcannon.com>
2024-05-31 18:39:11 +1200

    Revert test

--- utils/generateLocales.cjs
+++ utils/generateLocales.cjs
@@ -131,7 +131,7 @@ async function main(locale) {
               // Preserve the old translation if there is one
               localeData[keyName] = {
                 original: baseFileData[keyName]?.original,
-                value: md.render(oldLocaleData[keyName]?.value),
+                value: oldLocaleData[keyName]?.value,
               };
             }
           } else if (
